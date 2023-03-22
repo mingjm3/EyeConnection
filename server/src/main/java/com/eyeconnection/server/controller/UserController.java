d3ba639867372fc8e49ae4676031cf7d349e9b64
Arcgo <imingjun.ma@outlook.com>
2023-03-21 23:43:36 -0700

    finish sign_up post api with checking if user already exists

--- server/src/main/java/com/eyeconnection/server/controller/UserController.java
+++ server/src/main/java/com/eyeconnection/server/controller/UserController.java
@@ -1,9 +1,9 @@
 package com.eyeconnection.server.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.eyeconnection.server.dao.UserRepository;
 import com.eyeconnection.server.entity.User;
@@ -17,7 +17,16 @@ public class UserController {
     }
 
     @PostMapping("/sign_up")
-    public @ResponseBody User signUp(@RequestBody User user) {
-        return userRepo.save(user);
+    public ResponseEntity<?> signUp(@RequestBody User newUser) {
+        String email = newUser.getEmail();
+        User findResult = userRepo.findByEmail(email);
+        
+        //check if user is already exists
+        if(findResult != null) {
+            return ResponseEntity.status(202).body("User already exists with this email: " + email);
+        }
+
+        User savedUser = userRepo.save(newUser);
+        return ResponseEntity.status(201).body("User created successfully " + savedUser.getEmail());
     }
 }
