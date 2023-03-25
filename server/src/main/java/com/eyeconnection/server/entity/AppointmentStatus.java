5978904d1f20502b2eec834ee718e9f398fe8870
Arcgo <imingjun.ma@outlook.com>
2023-03-24 23:19:11 -0700

    define appointment status

---
+++ server/src/main/java/com/eyeconnection/server/entity/AppointmentStatus.java
@@ -0,0 +1,14 @@
+package com.eyeconnection.server.entity;
+
+public enum AppointmentStatus {
+    //Patient make an appointment and wait for doctor's confirmation
+    PENDING,
+    //Doctor have confirmed the appointment
+    APPROVED,
+    //Appointment have finished
+    FINISHED,
+    //Appointmnet have been cancelled by patients or doctors
+    CANCELLED,
+    //Appointmnet have error or make failed
+    ABORTED;
+}
